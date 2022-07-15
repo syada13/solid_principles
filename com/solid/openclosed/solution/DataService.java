85606f6db019c0661a2734001cfdd3443354232e
syadav <syadav@gozego.com>
2022-07-15 12:10:42 +0530

    Adding comments

--- com/solid/openclosed/solution/DataService.java
+++ com/solid/openclosed/solution/DataService.java
@@ -1,7 +1,10 @@
 package com.solid.openclosed.solution;
 
 import java.util.Map;
-
+/*
+ * Enabling the Open-Closed Principle:
+ * Firstly, and thinking abstractly, create an interface and define a contract that will include all required functionalities.
+ */
 public interface DataService {
     public Map<String, Long> getAPIPlans();
 }
